5ec02b6257688f1c902568781b4d60f4ffeb5d1a
Per Nilsson <per.nilsson@formidablelabs.com>
2015-11-23 09:41:36 -0800

    Fix lint

--- components/root.jsx
+++ components/root.jsx
@@ -3,7 +3,6 @@ import { Routehandler } from 'react-router';
 
 class Root extends React.Component {
   render() {
-    console.log(this.props.children);
     return (
       <div>
         {this.props.children}
